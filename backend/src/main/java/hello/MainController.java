3794fc8627688374795d6522a08bde050764520a
Nursultan <nursultan_imanov@mail.ru>
2018-12-25 23:39:42 +0100

    allow cors for get all method

--- backend/src/main/java/hello/MainController.java
+++ backend/src/main/java/hello/MainController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 import hello.User;
 import hello.UserRepository;
 
-@Controller    // This means that this class is a Controller
+@RestController    // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
 	@Autowired // This means to get the bean called userRepository
@@ -30,6 +32,7 @@ public class MainController {
 		return "Saved";
 	}
 
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		// This returns a JSON or XML with the users
