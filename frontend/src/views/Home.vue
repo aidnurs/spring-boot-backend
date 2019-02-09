feffc6ae2cf216f9a3d2d4475d51a657577665f1
Nursultan <nursultan_imanov@mail.ru>
2019-02-09 23:15:54 +0100

    [FrontEnd] change and delete todos methods

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
-    <div class="" v-for="todo in this.todos">
-      {{todo}}
+    <div class="" v-for="(todo,key) in this.todos">
+      <!--{{todo}}-->
+      {{todo.id}}
       {{todo.text}}
       <input type="checkbox" name="" value="" :checked="todo.done">
-    </div>
-    <!--<form class="" action="index.html" method="post" @submit.prevent="addNewTodo">
+      <button type="button" class="close" aria-label="Close" @click="deleteTodo(todo.id,key)">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>
+    <form class="" method="post" @submit.prevent="addNewTodo">
       <label for="text">text</label>
-      <input type="text" name="text" placeholder="text">
+      <input type="text" name="text" placeholder="text" v-model="text">
       <label for="done">done</label>
-      <input type="checkbox" name="done">
+      <input type="checkbox" name="done" v-model="done">
       <button type="submit" name="button">Submit</button>
-    </form>-->
+    </form>
   </div>
 </template>
 
@@ -25,7 +29,35 @@ export default Vue.extend({
   data() {
     return {
       todos: [],
+      text: '',
+      done: false,
     };
+  },
+  methods: {
+    addNewTodo() {
+      const jsonToSend = { text: this.text, done: this.done };
+      axios({
+        url: 'http://localhost:8090/todos/add',
+        method: 'post',
+        data: jsonToSend,
+      }).then(() => {
+        this.todos.push(jsonToSend);
+      });
+    },
+    changeTodo() {
+      axios({
+        url: 'http://localhost:8090/todos/change/' + id,
+        method: 'put',
+      }).then(() => {});
+    },
+    deleteTodo(id: number, key: number) {
+      axios({
+        url: 'http://localhost:8090/todos/delete/' + id,
+        method: 'delete',
+      }).then(() => {
+        this.todos.splice(key, 1);
+      });
+    },
   },
   mounted() {
     axios.get('http://localhost:8090/todos/get').then((response) => {
