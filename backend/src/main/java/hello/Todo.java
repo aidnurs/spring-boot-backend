6903e7f0fd6d5dedc08f5617284e3845329683f5
Nursultan <nursultan_imanov@mail.ru>
2019-02-09 21:19:16 +0100

    [BackEnd] delete user repository

--- backend/src/main/java/hello/Todo.java
+++ backend/src/main/java/hello/Todo.java
@@ -8,35 +8,35 @@ import javax.persistence.Id;
 @Entity // This tells Hibernate to make a table out of this class
 public class Todo {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String text;
 
     private Boolean done;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getText() {
-		return text;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public void setText(String text) {
+        this.text = text;
+    }
 
-	public Boolean getDone() {
-		return done;
-	}
+    public Boolean getDone() {
+        return done;
+    }
 
-	public void setDone(Boolean done) {
-		this.done = done;
-	}
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
 
 }
--- backend/src/main/java/hello/TodoRepository.java
+++ backend/src/main/java/hello/TodoRepository.java
@@ -2,8 +2,6 @@ package hello;
 
 import org.springframework.data.repository.CrudRepository;
 
-import hello.Todo;
-
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
